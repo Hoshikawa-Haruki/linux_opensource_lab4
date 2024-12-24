2a6e4e68e055631a7d7be44ce77b789ca6215e3f
Hoshikawa-Haruki <hairtail0524@gmail.com>
2024-12-24 14:23:51 +0900

    계산기 구현 완료

--- 9_gtk/calculator.c
+++ 9_gtk/calculator.c
@@ -1,92 +1,79 @@
 #include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+
 char current_input[256] = "";
 double result = 0;
 char operation = '\0';
 
-void on_button_clicked(GtkWidget *widget, gpointer entry)
-{
+void on_button_clicked(GtkWidget *widget, gpointer user_data) {
+    GtkEntry *entry = GTK_ENTRY(user_data);
     const char *label = gtk_button_get_label(GTK_BUTTON(widget));
-    const char *current_text = gtk_entry_get_text(GTK_ENTRY(entry));
-    if (g_strcmp0(label, "C") == 0)
-    {
+
+    const char *current_text = gtk_entry_get_text(entry);
+    if (g_strcmp0(label, "C") == 0) {
         result = 0;
         operation = '\0';
         current_input[0] = '\0';
-        gtk_entry_set_text(GTK_ENTRY(entry), "");
-    }
-    else if (g_strcmp0(label, "=") == 0)
-    {
+        gtk_entry_set_text(entry, "");
+    } else if (g_strcmp0(label, "=") == 0) {
         double value = atof(current_input);
-        if (operation == '+')
-            result += value;
-        else if (operation == '-')
-            result -= value;
-        else if (operation == '*')
-            result *= value;
-        else if (operation == '/')
-            result /= value;
+        if (operation == '+') result += value;
+        else if (operation == '-') result -= value;
+        else if (operation == '*') result *= value;
+        else if (operation == '/') result /= value;
+
         char buffer[256];
         snprintf(buffer, sizeof(buffer), "%.2f", result);
-        gtk_entry_set_text(GTK_ENTRY(entry), buffer);
+        gtk_entry_set_text(entry, buffer);
+
         current_input[0] = '\0';
         operation = '\0';
-    }
-    else if (g_strcmp0(label, "+") == 0 || g_strcmp0(label, "-") == 0 || g_strcmp0(label, "*") == 0 || g_strcmp0(label, "/") == 0)
-    {
-        if (current_input[0] != '\0')
-        {
+    } else if (strchr("+-*/", label[0])) {
+        if (current_input[0] != '\0') {
             result = atof(current_input);
             operation = label[0];
             current_input[0] = '\0';
         }
-    }
-    else
-    {
+    } else {
         strncat(current_input, label, sizeof(current_input) - strlen(current_input) - 1);
-        gtk_entry_set_text(GTK_ENTRY(entry), current_input);
+        gtk_entry_set_text(entry, current_input);
     }
 }
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
     gtk_init(&argc, &argv);
 
-    // 윈도우 생성
-    GtkWidget *window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "GTK+ 계산기");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 350);
-    gtk_container_set_border_width(GTK_CONTAINER(window), 10);
+    // Load the UI from XML
+    GtkBuilder *builder = gtk_builder_new_from_file("calculator.xml");
+
+    // Get main window and connect "destroy" signal
+    GtkWidget *window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // 메인 박스
-    GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 10);
-    gtk_container_add(GTK_CONTAINER(window), vbox);
+    // Get entry widget
+    GtkWidget *entry = GTK_WIDGET(gtk_builder_get_object(builder, "entry"));
 
-    // 텍스트 입력 상자
-    GtkWidget *entry = gtk_entry_new();
-    gtk_entry_set_alignment(GTK_ENTRY(entry), 1); // 오른쪽 정렬
-    gtk_widget_set_size_request(entry, 200, 50);
-    gtk_box_pack_start(GTK_BOX(vbox), entry, FALSE, FALSE, 5);
-
-    // 버튼 그리드
-    GtkWidget *grid = gtk_grid_new();
-    gtk_grid_set_row_spacing(GTK_GRID(grid), 5);
-    gtk_grid_set_column_spacing(GTK_GRID(grid), 5);
-    gtk_box_pack_start(GTK_BOX(vbox), grid, TRUE, TRUE, 0);
-
-    // 버튼 배열
-    const char *buttons[] = {"7", "8", "9", "+", "4", "5", "6", "-", "1", "2", "3", "*", "C", "0", "=", "/"};
-    for (int i = 0; i < 16; i++)
-    {
-        GtkWidget *button = gtk_button_new_with_label(buttons[i]);
-        gtk_widget_set_size_request(button, 63, 63);
-        gtk_grid_attach(GTK_GRID(grid), button, i % 4, i / 4, 1, 1);
+    // Connect buttons to the signal handler
+    for (int i = 0; i <= 9; i++) {
+        char button_id[20];
+        snprintf(button_id, sizeof(button_id), "button_%d", i);
+        GtkWidget *button = GTK_WIDGET(gtk_builder_get_object(builder, button_id));
         g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), entry);
     }
 
-    // 윈도우 표시
+    const char *operators[] = {"plus", "minus", "multiply", "divide", "clear", "equals"};
+    for (int i = 0; i < 6; i++) {
+        char button_id[20];
+        snprintf(button_id, sizeof(button_id), "button_%s", operators[i]);
+        GtkWidget *button = GTK_WIDGET(gtk_builder_get_object(builder, button_id));
+        g_signal_connect(button, "clicked", G_CALLBACK(on_button_clicked), entry);
+    }
+
+    // Show the window and run the GTK main loop
     gtk_widget_show_all(window);
     gtk_main();
 
+    g_object_unref(builder);
     return 0;
 }
